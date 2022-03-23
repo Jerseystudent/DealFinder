408c6ecd35b623daa47ad3765fc00a32f1c34863
Jerseystudent <90096181+Jerseystudent@users.noreply.github.com>
2022-03-23 19:49:00 +0000

    Update DealFinder.py
    Changing some comments

--- DealFinder.py
+++ DealFinder.py
@@ -1,3 +1,5 @@
+#original code and comments from Swanhack, heavily modified and coded further by Jerseystudent
+
 # Import the pycord library into your runtime. 
 import discord
 
@@ -5,9 +7,7 @@ import discord
 # All discord actions will act on this variable.
 client = discord.Client()
 
-# This is a function.
-# It executes whenever the bot has successfully initialised. 
-# In this case, it simply prints "We have logged in ..." to the terminal.
+# Successful log into discord via api key
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
